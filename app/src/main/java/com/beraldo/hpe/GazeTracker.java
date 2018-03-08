609f22ba198a770800f3e2fa88f1aed25dfc2bd1
Malhar <malhar.jajoo14@ic.ac.uk>
2018-03-08 17:29:45 +0000

    Added study session classes

---
+++ app/src/main/java/com/beraldo/hpe/GazeTracker.java
@@ -0,0 +1,40 @@
+package com.app.studybuddy;
+
+import java.util.Random;
+
+/**
+ * Created by malhar on 2/13/18.
+ */
+
+
+
+public class GazeTracker
+{
+
+    public enum GazeState
+    {
+        PRESENT_FOCUSSED, PRESENT_DISTRACTED, NOT_PRESENT;
+    }
+
+    // This will return a GazeTracker object
+    public GazeState getData(int i)
+    {
+        GazeState gazeState;
+
+        // some kind of logic to return different data.
+        if( i%2 == 0)
+        {
+            gazeState = GazeState.PRESENT_FOCUSSED;
+        }else if( i%3 == 0)
+        {
+            gazeState = GazeState.PRESENT_DISTRACTED;
+        }
+        else
+        {
+            gazeState = GazeState.NOT_PRESENT;
+        }
+
+        // fills in the gaze data.
+        return gazeState;
+    }
+}
---
+++ app/src/main/java/com/beraldo/hpe/StudySession.java
@@ -0,0 +1,13 @@
+package com.beraldo.hpe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudySession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studysession);
+    }
+}
