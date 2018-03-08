3089922eafcb72f1b81b105b9667532f9cc7faaf
Steve <s.karolcik@gmail.com>
2018-03-08 23:14:19 +0000

    Added session history window

---
+++ app/src/main/java/com/app/studybuddy/SessionHistory.java
@@ -0,0 +1,13 @@
+package com.app.studybuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SessionHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_history);
+    }
+}
